65f93d78e6b13880ca5c820e23440cdf7937a0a6
Asmahul Husna <ekra.cse8.bu@gmail.com>
2023-08-15 23:21:04 +0600

    week 10 Day 5

---
+++ week 10/Day 5/GCD Problem.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+#define gcd(a,b)  __gcd(a,b)
+#define lcm(a,b) (a*b)/__gcd(a,b)
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template <typename T> using pbds = tree<T, null_type, less<T>,rb_tree_tag, tree_order_statistics_node_update>;
+int mod = 1e9 + 7;
+const int N=1e5;
+
+int main()
+{
+    optimize();
+    ll t; cin >> t;
+    while(t--) {
+        ll n; cin >> n;
+        ll val = n - 1;
+        if(val%2) {
+            ll res = val/2;
+            ll div = val%2;
+            cout << res+div << ' '<< res << ' '<< 1 << '\n';
+        }
+        else {
+            ll res = val/2;
+            if(res%2)
+                cout << res+2 << ' '<< res-2 << ' '<< 1 << '\n';
+            else cout << res+1 << ' '<< res-1 << ' '<< 1 << '\n';
+        }
+    }
+}
+
